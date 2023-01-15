471e594004a9a2354f115ae9155bcc016612a56a
Toby Dunn <toby@timehri.world>
2023-01-15 01:19:23 +0000

    Tidy up a bit

--- spotify_pattern.py
+++ spotify_pattern.py
@@ -1,5 +1,6 @@
 from typing import List
 from collections import defaultdict
+from itertools import combinations
 
 
 def most_visited_pattern(
@@ -16,16 +17,21 @@ def most_visited_pattern(
         i.e. the sequence of websites which received the most amount  visits in total
 
     Example:
+         Input:
 
-        username = ["joe","joe","joe","james","james","james","james","mary","mary","mary"],
-        timestamp = [1,2,3,4,5,6,7,8,9,10],
-        website = ["home","about","career","home","cart","maps","home","home","about","career"]
+            username = ["joe","joe","joe","james","james","james","james","mary","mary","mary"],
+            timestamp = [1,2,3,4,5,6,7,8,9,10],
+            website = ["home","about","career","home","cart","maps","home","home","about","career"]
 
-        Output: ["home","about","career"]
+        Output:
+            ["home","about","career"]
 
     Explanaintion:
         Joe and Mary both visited home, about and career twice. This is the highest number
         of sequential visits
+
+    Raises:
+        N/A
     """
     # outline
     # get the triples of users who visited websites and at which time
@@ -35,20 +41,34 @@ def most_visited_pattern(
     # group by pattern -> count equivilant
     # find highest count and return pattern
 
-    # This assumes that the lists are sorted in time order
     # for each  username
     # while the timestamp is increasing
     #  find every 3pattern
-    #  add that pattern to a map
+    #  add that pattern to a hashmap
     #  continue over entire list of tuples until we've mapped every sequence
     #
     # if all visits are ordered by user we can use a deque -> sliding window
 
-    triple = list(zip(username, timestamp, website))
-    site_mapping = defaultdict(lambda: 0)
 
+    # sort via username and timestamp - this will group users together so we can get sequences
+    triple = sorted(zip(username, timestamp, website), key=lambda x: (x[0],x[1]))
 
-    return site_mapping
+    # create a mapping for each user with an ordered list of its site visits
+    site_mapping = defaultdict(lambda: [])
+
+    # basically for each username add its visisted site to a list of sites it visited
+    # then translate this list and find all sequential combinations of it
+
+    for username, timestamp, website in triple:
+        site_mapping[username].append(website)
+
+    pattern_visits = defaultdict(lambda: 0)
+
+    for username, visits in site_mapping.items():
+        for combination in combinations(visits, 3):
+            pattern_visits[combination] +=1
+
+    return max(pattern_visits.items(), key=lambda x: x[1])
 
 
 def test_most_visited_pattern() -> None:
